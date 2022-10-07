7f7111c17af6cb02040fab4a06eb81eb28ea6b87
Bernard <kimaryobernard@gmail.com>
2022-10-07 09:10:56 +0300

    first commit

---
+++ output_Escape_squence.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+cout<<"       _____           ///---\\\\\\ \n      /     \\          //     \\\\ \n      | O O |          /| O O |\\  \n      | <-> |          || <-> || \n      \\     /           \\     / \n       -----             ----- "<<endl; //Head
+cout<<"         |                 | "<<endl; //Neck
+cout<<"    /----|-----\\      /----|-----\\ "<<endl; //hands
+cout<<"         |                 |\n         |                 | "<<endl;//Body
+cout<<"        / \\               / \\\n       /   \\             /   \\\n       |   |            /     \\\n       |   |            -------\n       |   |             |   |\n       >>> >>>           >>  >>  \n"; //Legs
+system("pause");
+}
